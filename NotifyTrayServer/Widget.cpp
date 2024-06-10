2bdc525e59edf7fa8f37e2862c410c7aed145350
博丽幻月 <cqwlxxl@qq.com>
2024-06-10 13:41:32 +0800

    Socket服务端

--- NotifyTrayServer/Widget.cpp
+++ NotifyTrayServer/Widget.cpp
@@ -2,7 +2,10 @@
 #include "ui_Widget.h"
 
 #include <QTimer>
-#include <QDebug>
+#include <QTcpSocket>
+#include <QDataStream>
+#include <QMessageBox>
+#include <QNetworkProxy>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -16,7 +19,44 @@ Widget::Widget(QWidget *parent)
 Widget::~Widget()
 {
     uninstallHook_NotifyWndProc();
+    foreach(QTcpSocket *socket, mConnectionSet)
+    {
+        socket->close();
+        socket->deleteLater();
+    }
+    if(mServer)
+    {
+        mServer->close();
+    }
     delete ui;
+}
+
+///新的连接
+void Widget::slotNewConnection()
+{
+    logit(tr("发现新的客户端连接"));
+    while(mServer->hasPendingConnections())
+    {
+        QTcpSocket *socket = mServer->nextPendingConnection();
+//        socket->setProxy(QNetworkProxy::NoProxy);
+        mConnectionSet.insert(socket);
+        connect(socket, &QTcpSocket::disconnected, this, &Widget::slotDiscardSocket, Qt::UniqueConnection);
+        logit(QString(tr("客户端[socket:%1]已连接")).arg(socket->socketDescriptor()));
+    }
+}
+
+///断开socket
+void Widget::slotDiscardSocket()
+{
+    QTcpSocket *socket = reinterpret_cast<QTcpSocket*>(sender());
+    QSet<QTcpSocket*>::iterator it = mConnectionSet.find(socket);
+    if(it != mConnectionSet.end())
+    {
+        logit(tr("一个客户端已断开"));
+        mConnectionSet.remove(*it);
+    }
+
+    socket->deleteLater();
 }
 
 ///初始化
@@ -27,8 +67,30 @@ void Widget::haku()
     mDwMessage.insert(NIM_DELETE,       "NIM_DELETE");
     mDwMessage.insert(NIM_SETFOCUS,     "NIM_SETFOCUS");
     mDwMessage.insert(NIM_SETVERSION,   "NIM_SETVERSION");
+    mMSD[IdWeChat].appName = tr("微信");
+    mMSD[IdQQ].appName = tr("QQ");
+    mMSD[IdCloudHub].appName = tr("云之家");
+    mMSD[IdDingTalk].appName = tr("钉钉");
 
-    qDebug() << "HOOK result: " << installHook_NotifyWndProc((HWND)this->winId());
+    logit(QString("安装钩子%1").arg(installHook_NotifyWndProc((HWND)this->winId())?tr("成功"):tr("失败")));
+
+    mServer = new QTcpServer(this);
+//    mServer->setProxy(QNetworkProxy::NoProxy);
+    if(mServer->listen(QHostAddress::Any, 8080))
+    {
+        connect(mServer, &QTcpServer::newConnection, this, &Widget::slotNewConnection, Qt::UniqueConnection);
+        logit(tr("服务已开启，监听客户端连接中..."));
+    }
+    else
+    {
+        logit(tr("无发开启服务"));
+    }
+}
+
+///打印log
+void Widget::logit(QString str)
+{
+    ui->textBrowser->append(QString("[%1] %2").arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz"), str));
 }
 
 ///安装 Win32 钩子
@@ -38,19 +100,19 @@ bool Widget::installHook_NotifyWndProc(HWND hWnd)
     HWND hShellWnd = ::FindWindow(TEXT("Shell_TrayWnd"), nullptr);
     if(!hShellWnd || !::IsWindow(hShellWnd))
     {
-        qDebug() << "Not found Shell_TrayWnd";
+        logit(tr("无法找到托盘[Shell_TrayWnd]句柄"));
         return false;
     }
 
     mCallWndProcHooked = InstallCallWndProcHook(hWnd, hShellWnd);
     if(mCallWndProcHooked)
     {
-        qDebug() << "Hook CallWndProc OK!";
+        logit(tr("安装钩子[CallWndProc]成功"));
         return true;
     }
     else
     {
-        qDebug() << "Hook CallWndProc FAILED";
+        logit(tr("安装钩子[CallWndProc]失败"));
         return false;
     }
 }
@@ -59,7 +121,97 @@ bool Widget::installHook_NotifyWndProc(HWND hWnd)
 void Widget::uninstallHook_NotifyWndProc()
 {
     UninstallCallWndProcHook();
-    qDebug() << "Unhook CallWndProc";
+    logit(tr("已卸载钩子[CallWndProc]"));
+}
+
+///发送socket消息
+void Widget::sendMessage(QTcpSocket *socket, QString str)
+{
+    if(socket)
+    {
+        if(socket->isOpen())
+        {
+            QDataStream socketStream(socket);
+            socketStream.setVersion(QDataStream::Qt_5_12);
+
+            QByteArray header;
+            header.prepend(QString("fileType:message,fileSize:%1;").arg(str.size()).toUtf8());
+            header.resize(128);
+
+            QByteArray byteArray = str.toUtf8();
+            byteArray.prepend(header);
+
+            socketStream.setVersion(QDataStream::Qt_5_12);
+            socketStream << byteArray;
+        }
+        else
+        {
+            QMessageBox::critical(this, "错误", "Socket未打开");
+        }
+    }
+    else
+    {
+        QMessageBox::critical(this, "错误", "Socket未连接");
+    }
+}
+
+///广播socket消息
+void Widget::broadcastMessage(QString str)
+{
+    foreach(QTcpSocket *socket, mConnectionSet)
+    {
+        sendMessage(socket, str);
+    }
+}
+
+///处理图标信息
+void Widget::taryInfo(QString type, ulong iconId, QString title)
+{
+    logit(QString("type=%1, iconId=%3, title=%2").arg(type, title).arg(iconId));
+    if(type == mDwMessage[NIM_MODIFY])
+    {   //图标更改
+        if(title.contains(mMSD[IdWeChat].appName))
+        {
+            checkFlash(IdWeChat, iconId);
+        }
+        else if(title.contains(mMSD[IdCloudHub].appName))
+        {
+            checkFlash(IdCloudHub, iconId);
+        }
+        else if(title.contains(mMSD[IdDingTalk].appName))
+        {
+            checkFlash(IdDingTalk, iconId);
+        }
+    }
+}
+
+///检查软件
+void Widget::checkFlash(int appId, ulong iconId)
+{
+    if(appId < IdWeChat || appId >= IdMax || appId == IdQQ)
+    {   //检查appId有效性，QQ的检测方法暂未实现
+        return;
+    }
+    QDateTime datetime = QDateTime::currentDateTime();
+    if(mMSD[appId].lastModifyTime.msecsTo(datetime) <= 1000 && mMSD[appId].lastIconId != iconId)
+    {   //小于1000ms，且图标ID有变化
+        mMSD[appId].modifyCount++;
+    }
+    else
+    {
+        mMSD[appId].modifyCount = 0;
+    }
+    mMSD[appId].lastModifyTime = datetime;
+    mMSD[appId].lastIconId = iconId;
+    //
+    if(mMSD[appId].modifyCount >= 3)
+    {   //3次有效则认为在闪动
+        broadcastMessage(QString("%1,1,%2").arg(appId).arg(iconId != 0));
+    }
+    else
+    {
+        broadcastMessage(QString("%1,0,1").arg(appId));
+    }
 }
 
 bool Widget::nativeEvent(const QByteArray &eventType, void *message, long *result)
@@ -78,33 +230,12 @@ bool Widget::nativeEvent(const QByteArray &eventType, void *message, long *resul
         {
             break;
         }
-        qDebug() << "lpReceiveCDS->cbData: " << lpReceiveCDS->cbData << "sizeof(TRAY_ICON_DATAW): " << sizeof(TRAY_ICON_DATAW);
-
-        //测试时，只实现了 WM_NotifyCallWndProc 钩子的消息处理
+        logit(QString("lpReceiveCDS->cbData=%1, sizeof(TRAY_ICON_DATAW)=%2").arg(lpReceiveCDS->cbData).arg(sizeof(TRAY_ICON_DATAW)));
         if(lpReceiveCDS->dwData == WM_NotifyCallWndProc && lpReceiveCDS->cbData == sizeof(TRAY_ICON_DATAW))
         {
             lpNotifyData = (TRAY_ICON_DATAW*)lpReceiveCDS->lpData;
-
-            //输出结果
-            qDebug() << QString("CTray-NotifyIconMsg:[%1]:[HWND:0x%2]").arg(mDwMessage[lpNotifyData->dwMessage]).arg(QString::number(pMsg->wParam, 16));
-            qDebug() << "icon_id: " << lpNotifyData->uIconID << " title: " << QString::fromWCharArray(lpNotifyData->szTip);
-
-            if((lpNotifyData->uFlags & NIF_INFO) != 0)
-            {
-                qDebug() << QString("Tip[%1], szInfoParam:").arg(QString::fromWCharArray(lpNotifyData->szTip));
-                qDebug() << QString("InfoTitle[%1], Info[%2], InfoFlags[%3]")
-                            .arg(QString::fromWCharArray(lpNotifyData->szInfoTitle),
-                                 QString::fromWCharArray(lpNotifyData->szInfo),
-                                 QString::number(lpNotifyData->dwInfoFlags));
-            }
-            else if((lpNotifyData->uFlags & NIF_TIP) != 0)
-            {
-                qDebug() << QString("Tip[%1], non-szInfo").arg(QString::fromWCharArray(lpNotifyData->szTip));
-            }
-            else
-            {
-                qDebug() << QString("non-szTip, non-szInfo");
-            }
+            logit(QString("[%1] => [HWND: 0x%2]").arg(mDwMessage[lpNotifyData->dwMessage], QString::number(pMsg->wParam, 16)));
+            taryInfo(mDwMessage[lpNotifyData->dwMessage], lpNotifyData->uIconID, QString::fromWCharArray(lpNotifyData->szTip));
         }
     }
         break;
@@ -114,4 +245,3 @@ bool Widget::nativeEvent(const QByteArray &eventType, void *message, long *resul
 
     return QWidget::nativeEvent(eventType, message, result);
 }
-
--- NotifyTrayServer/Widget.h
+++ NotifyTrayServer/Widget.h
@@ -3,7 +3,8 @@
 
 #include <QWidget>
 #include <QMap>
-#include <QWidget>
+#include <QTcpServer>
+#include <QDateTime>
 
 #include <windows.h>
 #include "../LibTrayHook/LibTrayHook/trayhook.h"
@@ -11,6 +12,23 @@
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
+
+enum AppId
+{
+    IdWeChat = 0,       //微信
+    IdQQ,               //QQ
+    IdCloudHub,         //云之家
+    IdDingTalk,         //钉钉
+    IdMax               //分界线
+};
+
+struct ModifySocketData
+{
+    QString     appName;
+    QDateTime   lastModifyTime {QDateTime::currentDateTime()};
+    ulong       lastIconId {0};
+    int         modifyCount {0};
+};
 
 class Widget : public QWidget
 {
@@ -23,15 +41,27 @@ public:
 private:
     Ui::Widget *ui;
 
+private slots:
+    void slotNewConnection();       //新的连接
+    void slotDiscardSocket();       //断开socket
+
 private:
     void haku();                                        //初始化
+    void logit(QString str);                            //打印log
     bool installHook_NotifyWndProc(HWND hWnd);          //安装 Win32 钩子
     void uninstallHook_NotifyWndProc();                 //卸载 Win32 钩子
+    void sendMessage(QTcpSocket* socket, QString str);  //发送socket消息
+    void broadcastMessage(QString str);                 //广播socket消息
+    void taryInfo(QString type, ulong iconId, QString title);       //处理图标信息
+    void checkFlash(int appId, ulong iconId);                       //检查是否闪动
 
 protected:
     bool nativeEvent(const QByteArray &eventType, void *message, long *result);
 
 private:
     QMap<int, QString>      mDwMessage;
+    QTcpServer              *mServer;
+    QSet<QTcpSocket*>       mConnectionSet;
+    ModifySocketData        mMSD[IdMax];
 };
 #endif // WIDGET_H
